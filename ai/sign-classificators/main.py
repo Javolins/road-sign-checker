9abc62d59344cf183c5823cc2fa74712f964d84d
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-01-14 10:04:28 +0100

    display loaded image

--- ai/sign-classificators/main.py
+++ ai/sign-classificators/main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+import os.path
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,8 +13,14 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    znakiDirPath = "../../znaki-sandbox/znaki"
-    znakPath = znakiDirPath + "/D-3.png"
-    print(znakPath)
+    reposDir = os.path.abspath('./../../..')
+    znakiDir = os.path.abspath(reposDir + '/znaki-sandbox/znaki')
+    znakPath = os.path.abspath(znakiDir + "/D-3.png")
+
+    
+    znakImage = cv2.imread(znakPath)
+    cv2.imshow("znak", znakImage)
+    cv2.waitKey(0)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
