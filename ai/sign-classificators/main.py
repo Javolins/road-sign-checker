6fb82c87d4bcab1c8d37b786e0dfccf8fac7c8a8
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-01-14 18:40:08 +0100

    refactored script to accept image from CLI updated requirements added comments and questions

--- ai/sign-classificators/main.py
+++ ai/sign-classificators/main.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+
 from scipy.stats import circmean
-from PIL import Image
-import glob
+
+import sys
+import os
 
 BLACK_THRESHOLD_VALUE = 76
 WHITE_THRESHOLD_VALUE_MIN = 140
@@ -30,9 +31,12 @@ def getParametersFromImage(HSV_image):
 def findMainColor(HSV_image):
     image_size, fill_ratio = getParametersFromImage(HSV_image)
     middle_px = image_size // 2
+    #what about image height?
     counter = 0
     distance = 0
     hsv_list = []
+    #collect colorful(non-black and non-white) pixels starting from center
+    #procedure iterates through wider and wider squares until a certain amount is collected
     while(counter < COLORFUL_PIXELS_REQUIRED):
         for i in range(middle_px - distance, middle_px + distance + 1):
             for j in range(middle_px - distance, middle_px + distance + 1):
@@ -63,34 +67,58 @@ def isWhite(HSV_value):
     return HSV_value[2] > WHITE_THRESHOLD_VALUE_MIN and HSV_value[1] < WHITE_THRESHOLD_SATURATION_MAX
     
 def makeMask(HSV_image, HSV_mean):
-    
+
+    SOME_MAGIC_HUE_THRESHOLD = 179
+
     lower_hue = HSV_mean[0] - HUE_MARGIN
     if lower_hue < 0:
-        lower_hue += 179
-    if lower_hue > 179:
-        lower_hue -= 179
+        lower_hue += SOME_MAGIC_HUE_THRESHOLD
+    if lower_hue > SOME_MAGIC_HUE_THRESHOLD:
+        lower_hue -= SOME_MAGIC_HUE_THRESHOLD
     
     upper_hue = HSV_mean[0] + HUE_MARGIN
     if upper_hue < 0:
-        upper_hue += 179
-    if upper_hue > 179:
-        upper_hue -= 179
+        upper_hue += SOME_MAGIC_HUE_THRESHOLD
+    if upper_hue > SOME_MAGIC_HUE_THRESHOLD:
+        upper_hue -= SOME_MAGIC_HUE_THRESHOLD
     
     lowerBound = (lower_hue, HSV_mean[1] - SATURATION_MARGIN_MIN, HSV_mean[2] - VALUE_MARGIN_MIN)
     upperBound = (upper_hue, 255, 255)
     
     if lowerBound[0] > upperBound[0]:
         mask1 = cv2.inRange(HSV_image, lowerb=(0, lowerBound[1], lowerBound[2]), upperb = upperBound) 
-        mask2 = cv2.inRange(HSV_image, lowerb=lowerBound, upperb = (179, upperBound[1], upperBound[2]))
+        mask2 = cv2.inRange(HSV_image, lowerb=lowerBound, upperb = (SOME_MAGIC_HUE_THRESHOLD, upperBound[1], upperBound[2]))
         mask = mask1 | mask2
     else:
         mask = cv2.inRange(HSV_image, lowerb = lowerBound, upperb = upperBound)
     return mask
     
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("provide image path")
+        exit(1)
 
-# main
-hue_mean, saturation_mean, value_mean = findMainColor(readImageAsHSV("znaki/A-7_2.jpg"))
-HSV_mean = (hue_mean, saturation_mean, value_mean)
-mask = makeMask(readImageAsHSV("znaki/A-7_2.jpg"), HSV_mean)
-plt.imshow(mask, cmap='gray')
-plt.savefig("aaaaaaa.jpg")
+    if not os.path.exists(sys.argv[1]):
+        print("file does not exist")
+        exit(2)
+
+    znakPath = sys.argv[1]
+
+    #does OpenCV return image as BGR by default?
+    znakImage = cv2.imread(znakPath)
+    if znakImage is None:
+        print("file is not an image")
+        exit(3)
+
+    cv2.imshow("znak", znakImage)
+
+    znakImageHSV = cv2.cvtColor(znakImage, cv2.COLOR_BGR2HSV)
+    cv2.imshow("znak HSV", znakImageHSV)
+
+    hue_mean, saturation_mean, value_mean = findMainColor(znakImageHSV)
+    HSV_mean = (hue_mean, saturation_mean, value_mean)
+    mask = makeMask(znakImageHSV, HSV_mean)
+
+    cv2.imshow("mask", mask)
+
+    cv2.waitKey(0)
