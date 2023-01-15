7cd322ddfb4114a221bb18d0fd47bd94dd70d1fd
CoreNest <ernestk@student.agh.edu.pl>
2023-01-15 16:20:10 +0100

    estymator do kolorów

--- ai/sign-classificators/DominantColor.py
+++ ai/sign-classificators/DominantColor.py
@@ -15,37 +15,45 @@ class DominantColors:
 
 #make the white frame around photo first
     def dominantColors(self):
-        # read image
-        img = cv2.imread(self.IMAGE)
+        
+        img = self.IMAGE
 
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        # cv2.imshow('Result', hsv)
+        # cv2.waitKey(0)
+
         #deleting whit fragments
         lower = np.array([0, 0, 30])  # -- Lower range --
         upper = np.array([255, 255, 255])  # -- Upper range --
-        mask = cv2.inRange(img, lower, upper)
+        mask = cv2.inRange(hsv, lower, upper)
         res = cv2.bitwise_and(hsv, hsv, mask=mask)  # -- Contains pixels having the gray color--
-        cv2.imshow('Result', res)
-        cv2.waitKey(0)
+        # res = cv2.cvtColor(res, cv2.COLOR_HSV2RGB)
+        # cv2.imshow('Result', res)
+        # cv2.waitKey(0)
 
         # reshaping to a list of pixels
-        img = res.reshape((res.shape[1] * res.shape[0], 3))
+        res = res.reshape((res.shape[1] * res.shape[0], 3))
 
-
+        
 
         # save image after operations
         self.IMAGE = img
 
         # using k-means to cluster pixels
-        kmeans = KMeans(n_clusters=self.CLUSTERS)
-        kmeans.fit(img)
+        kmeans = KMeans(n_clusters=self.CLUSTERS, n_init='auto')
+        kmeans.fit(res)
 
         # the cluster centers are our dominant colors.
         self.COLORS = kmeans.cluster_centers_
 
         # returning after converting to integer from float
-        return self.COLORS.astype(int)
+        # res = cv2.cvtColor(res, cv2.COLOR_HSV2RGB)
+        kmeans.fit(res)
+        
+        return kmeans.cluster_centers_
 
-    def printCol(self):
+
+    def printCol(self, image_col):
 
         thickness = -1
 
@@ -55,13 +63,38 @@ class DominantColors:
         for i in range(self.CLUSTERS):
             start_point = (i*self.WIDTH, 0)
             end_point = ((i+1)*self.WIDTH, 200)
-            color = int(self.COLORS[i][0])/255,int(self.COLORS[i][1])/255,int(self.COLORS[i][2])/255
-            # print(color)
-            # color = (255, 0, 0)
-            image = cv2.rectangle(image, start_point, end_point, color, thickness)
-
+            color = np.uint8([[[ self.COLORS[i][0], self.COLORS[i][1],self.COLORS[i][2] ]]])
+            color = cv2.cvtColor(color, cv2.COLOR_HSV2RGB)
+            color = color[0][0]
+            col= ( (color[0])/255,(color[1])/255,(color[2])/255)
+            # print(col)
+            image = cv2.rectangle(image, start_point, end_point, col, thickness)
+        # print redHSV
         # image = cv2.rectangle(image, (0,0), (20,40),(0, 0, .7), thickness)
-        window_name = 'Colors'
-        cv2.imshow(window_name, image)
-        print(self.COLORS.astype(int));
-        cv2.waitKey(0)
+        # window_name = 'Colors'
+        image_col.append(image)
+        # print(self.COLORS.astype(int));
+        # cv2.waitKey(0)
+        
+if __name__ == '__main__': 
+    from matplotlib import pyplot as plt
+
+    "A-1.png"
+    glob_list = []
+    image_list = []
+    image_list_col = []
+    
+    filename= "../../znaki-sandbox/znaki/A-1.png"
+   
+    im = cv2.imread(filename)
+    # im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    image_list.append(im)
+        
+    img = image_list[0]
+    dc = DominantColors(img)
+    dmc = dc.dominantColors();# RGB
+    dc.printCol(image_list_col)
+
+    # image_list_col[0] = cv2.cvtColor(image_list_col[0], cv2.COLOR_RGB2BGR)
+    cv2.imshow('Result', image_list_col[0])
+    cv2.waitKey(0)
