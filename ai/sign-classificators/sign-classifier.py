9881c08224c2fc8232539d7b46493549b97a6aaa
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-01-20 18:33:54 +0100

    started working on sorting result

--- ai/sign-classificators/sign-classifier.py
+++ ai/sign-classificators/sign-classifier.py
@@ -3,6 +3,36 @@ import os
 
 from fastai.vision.all import *
 
+from enum import Enum;
+
+class SignType(Enum):
+    WARNING = 'warn'
+    PROHIBITION = 'proh'
+    WARRANT = 'warnt'
+    INFORMATIONAL = 'info'
+
+class SignsNeuralNetworkPathBuilers:
+    def __init__(self):
+        self.nnDirPath = os.path.abspath('nn')
+        self.datasetsDirName = 'datasets'
+        self.rawDatasetDirName = 'datasets'
+        self.preprocessedDatasetDirName = 'datasets'
+        self.learnerFilesDirName = 'learner'
+        self.modelsDirName = 'models'
+
+    def getModelPath(self, signType):
+        modelFilename = signType + '.pkl'
+        modePath = os.path.join(self.nnDirPath, self.modelsDirName, modelFilename)
+        return modelPath
+
+    def getRawDatasetDirPath(self, signType):
+        rawDatasetPath = os.path.join(self.nnDirPath, self.datasetsDirName, signType, self.rawDatasetDirName)
+        return rawDatasetPath
+
+    def getPreprocessedDatasetDirPath(self, signType):
+        preprossedDatasetPath = os.path.join(self.nnDirPath, self.datasetsDirName, signType, self.preprocessedDatasetDirName)
+        return preprossedDatasetPath
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("provide sign type")
@@ -28,3 +58,16 @@ if __name__ == '__main__':
     print(learner.dls.vocab)
     predictionResult = learner.predict(inputImagePath)
     print(predictionResult)
+
+    predictionDict = {}
+    vocabLength = len(learner.dls.vocab)
+    for dictElementIndex in range(vocabLength):
+        dictElement = learner.dls.vocab[dictElementIndex]
+        elementPredictionValue = predictionResult[2][dictElementIndex]
+        predictionDict[dictElement] = float(elementPredictionValue)
+
+    print(predictionDict)
+    sortedPredictions = dict(sorted(predictionDict.items(), key=lambda item: item[1], reverse=True))
+    print(sortedPredictions)
+
+    #posotruj predictions od największej do najmniejszej i zwróć posortowany słownik klasyfikacji
