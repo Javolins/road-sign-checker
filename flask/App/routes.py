b75d78e3fc0477375eec765e539916aca7760e23
P4ndaM1x <77618102+P4ndaM1x@users.noreply.github.com>
2023-01-21 19:07:21 +0000

    /api/info endpoint is working

--- flask/App/routes.py
+++ flask/App/routes.py
@@ -7,6 +7,7 @@ from flask_limiter.util import get_remote_address
 from threading import Thread
 from time import sleep
 import cv2
+import json
 
 from matplotlib import pyplot as plt
 
@@ -15,7 +16,6 @@ sys.path.insert(1, '/home/michal/repo/road-sign-checker/ai/sign-classificators/i
 from img_scripts import crop_image as ci
 from img_scripts import image_analisys_functions as iaf
 from img_scripts import classifier_color as cc
-import numpy
 
 limiter = Limiter(get_remote_address,app=app,storage_uri="memory://")
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -23,9 +23,23 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def pipeline(uid):
     filepath_img = "/".join([app.config['UPLOAD_FOLDER'], uid + '.jpg'])
     filepath_mask = "/".join([app.config['UPLOAD_FOLDER'], uid + '_mask.jpg'])
+    filepath_json = "/".join([app.config['DOWNLOAD_FOLDER'], uid + '.json'])
+    
     mask = ci.getFinalMaskFromImage(filepath_img)
     cv2.imwrite(filepath_mask, mask)
-    print(iaf.getShape(mask))
+    
+    info = {}
+    info['maskShape'] = str(iaf.getMaskShape(mask))
+    if info['maskShape'] == 'ZnakShape.OCTAGON' or info['maskShape'] == 'ZnakShape.UNKNOWN':
+        with open(filepath_json, "w") as outfile:
+            json.dump(info, outfile)
+        return
+    info['finalColorClasifaier'] = cc.finalColorClasifaier(ci.readImageAsRGB(filepath_img), mask)
+    
+    with open(filepath_json, "w") as outfile:
+        json.dump(info, outfile)
+    
+    print(info)
     #TODO
 
 @app.route('/')
@@ -40,13 +54,14 @@ def upload():
     if not os.path.isdir(app.config['UPLOAD_FOLDER']):
         os.mkdir(app.config['UPLOAD_FOLDER'])
     
-    print(request.files)
     if 'userImg' not in request.files:
         return jsonify({"info":"File key not found"})
     
     uid = str(uuid.uuid4())
         
     file = request.files['userImg']
+    print("DUUUPPAA")
+    print(file)
     filename = secure_filename(uid+'.jpg')
     destination="/".join([app.config['UPLOAD_FOLDER'], filename])
     file.save(destination)
@@ -66,25 +81,36 @@ def info():
         os.mkdir(app.config['DOWNLOAD_FOLDER'])
     
     uid = str(request.get_json()['uid'])
-    filepath = "/".join([app.config['DOWNLOAD_FOLDER'], uid + '.txt'])
+    filepath = "/".join([app.config['DOWNLOAD_FOLDER'], uid + '.json'])
     if not os.path.isfile(filepath):
         return jsonify({"info":"Info about uid='" + uid + "' not found"})
     
-    #TODO
+    fp = open(filepath)
+    info = json.load(fp)
 
-    return jsonify({"uid":uid})
+    return jsonify(info)
 
 
 def uposledzony_pipeline():
+    
+    dictionary = {}
+    
     filepath_img = '/home/michal/repo/road-sign-checker/flask/App/static/photos/6db324da-a096-4872-ba58-b1231f4badc1.jpg'
     filepath_mask = '/home/michal/repo/road-sign-checker/flask/App/static/photos/6db324da-a096-4872-ba58-b1231f4badc1_mask.jpg'
+    filepath_json = '/home/michal/repo/road-sign-checker/flask/App/static/info/6db324da-a096-4872-ba58-b1231f4badc1.json'
     mask = ci.getFinalMaskFromImage(filepath_img)
     cv2.imwrite(filepath_mask, mask)
-    # print(numpy.ndarray(mask))
-    # print(iaf.getShape(cv2.cvtColor(cv2.cvtColor(mask, cv2.COLOR_HSV2RGB), cv2.COLOR_RGBA2GRAY)))
-    dictionary = {}
+    dictionary['maskShape'] = str(iaf.getMaskShape(mask))
+    
+    if dictionary['maskShape'] == 'ZnakShape.OCTAGON' or dictionary['maskShape'] == 'ZnakShape.UNKNOWN':
+        with open(filepath_json, "w") as outfile:
+            json.dump(dictionary, outfile)
+        return
+        
     dictionary['finalColorClasifaier'] = cc.finalColorClasifaier(ci.readImageAsRGB(filepath_img), mask)
-    #TODO
+    
+    with open(filepath_json, "w") as outfile:
+        json.dump(dictionary, outfile)
 
 @app.route('/kurwamac', methods=['GET'])
 @limiter.limit("1/second")
